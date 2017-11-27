b97e8e9cfd08616a2c26ef4968b6a9c3b1510171
Graham Mosley <gmosley@seas.upenn.edu>
2017-11-27 17:18:56 -0500

    updates to the old prototype

--- prototype.py
+++ prototype.py
@@ -5,6 +5,7 @@ from collections import Counter
 
 infohashes = Counter()
 
+
 class DHTCrawler(btdht.DHT_BASE):
     def on_get_peers_query(self, query):
         try:
@@ -15,6 +16,7 @@ class DHTCrawler(btdht.DHT_BASE):
         except KeyError:
             pass
 
+
 dht = DHTCrawler(bind_port=40363, debuglvl=0)
 dht.register_message(b'get_peers')
 routing_table = dht.root
@@ -22,7 +24,9 @@ routing_table = dht.root
 dht.start()
 
 while True:
-    time.sleep(30)
+    time.sleep(10)
     stats = routing_table.stats()
     print('<dht_info> {} nodes ({} good), {} infohashes ({} unique)'
           .format(stats[0], stats[1], sum(infohashes.values()), len(infohashes)))
+    for info_hash, count in infohashes.most_common(3):
+        print(count, binascii.hexlify(info_hash))
