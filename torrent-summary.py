0d5f61ada19d257205edc4cffa9f97c35d902426
Graham Mosley <gmosley@seas.upenn.edu>
2018-01-29 23:23:45 -0500

    show more data

--- torrent-summary.py
+++ torrent-summary.py
@@ -4,10 +4,18 @@ import libtorrent as lt
 
 torrent_path = Path('torrents/')
 
+def human_readable_size(size, decimal_places=2):
+    for unit in ['B','KB','MB','GB','TB']:
+        if size < 1024.0:
+            break
+        size /= 1024.0
+    return f"{size:.{decimal_places}f}{unit}"
+
 for torrent_file in torrent_path.iterdir():
     if not torrent_file.name.endswith('.torrent'): continue
     with torrent_file.open('rb') as f:
         torrent = lt.bdecode(f.read())
+
         info = torrent[b'info']
 
         if b'name.utf-8' in info:
@@ -15,9 +23,17 @@ for torrent_file in torrent_path.iterdir():
         else:
             torrent_name = info[b'name'].decode()
 
-        print(torrent_name)
+        num_files = 1
+        size = 0
 
-        # print(info.keys())
-        # if b'files' in torrent[b'info']:
-        #     pprint(info[b'files'])
+        if b'length' in info:
+            num_files = 1
+            size = info[b'length']
+        else:
+            num_files = 0
+            for f in info[b'files']:
+                num_files += 1
+                size += f[b'length']
 
+        print('{} - {} ({})'.format(
+            torrent_name, human_readable_size(size), num_files))
