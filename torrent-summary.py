f3cb32f1cf745cc390b519913dd4851e7d6f0e15
Graham Mosley <gmosley@seas.upenn.edu>
2018-02-07 18:12:34 -0500

    improved summary script

--- torrent-summary.py
+++ torrent-summary.py
@@ -11,9 +11,13 @@ def human_readable_size(size, decimal_places=2):
         size /= 1024.0
     return f"{size:.{decimal_places}f}{unit}"
 
+def human_path(path):
+    return '/'.join([f.decode(errors='ignore') for f in path])
+
 for torrent_file in torrent_path.iterdir():
     if not torrent_file.name.endswith('.torrent'): continue
     with torrent_file.open('rb') as f:
+        print(torrent_file)
         torrent = lt.bdecode(f.read())
 
         info = torrent[b'info']
@@ -23,17 +27,25 @@ for torrent_file in torrent_path.iterdir():
         else:
             torrent_name = info[b'name'].decode()
 
+        print(torrent_name)
+
         num_files = 1
         size = 0
 
         if b'length' in info:
             num_files = 1
             size = info[b'length']
+            print('  {}'.format(info[b'name'].decode()))
         else:
             num_files = 0
             for f in info[b'files']:
                 num_files += 1
                 size += f[b'length']
+                print('  {} - {}'.format(
+                    human_path(f[b'path']),
+                    human_readable_size( f[b'length']))
+                )
+        print()
 
-        print('{} - {} ({})'.format(
-            torrent_name, human_readable_size(size), num_files))
+        # print('{} - {} ({})'.format(
+        #     torrent_name, human_readable_size(size), num_files))
