d0169fa3a90737e7738d9494130465bb4284701c
Dylan Mann <mannd@seas.upenn.edu>
2018-03-14 11:46:04 -0400

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+#Make sure that we clone/run as ubuntu
+su ubuntu
 cd ~
+
+# Get latest copy of mldht
 git clone https://github.com/dylanmann/mldht.git
 cd mldht
+
+# build code
 mvn package dependency:copy-dependencies appassembler:assemble && mvn antrun:run
+
+# run daemon as root
 cd work
 (../bin/mldht 0<&- &> out.log &) & 
