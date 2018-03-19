8ed42680f17bf349c37a9e89bef5bfb92847de8f
Graham Mosley <gmosley@seas.upenn.edu>
2018-03-18 22:37:48 -0400

    basic spark processing

---
+++ spark-scala/build.sbt
@@ -0,0 +1,7 @@
+name := "spark-scala"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.0"
---
+++ spark-scala/src/main/scala/SimpleApp.scala
@@ -0,0 +1,28 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "boonlog.json" // Should be some file on your system
+
+    val conf = new SparkConf()
+      .setMaster("local")
+      .setAppName("Simple Application")
+    val spark = SparkSession.builder.config(conf).getOrCreate()
+
+    val logData = spark.read.json(logFile).cache()
+
+    val count = logData.count()
+    println(s"Number of records: $count")
+
+
+    logData.filter(col("location.country").isNotNull)
+      .groupBy("location.country")
+      .count().sort(desc("count"))
+      .show(20)
+    logData.groupBy("type").count().show()
+
+    spark.stop()
+  }
+}
