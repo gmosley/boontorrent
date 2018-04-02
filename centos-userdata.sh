e06af18604b147aa2466fcee2d2d354f942d6d29
Dylan Mann <mannd@seas.upenn.edu>
2018-04-02 17:15:17 -0400

    Update centos-userdata.sh

--- centos-userdata.sh
+++ centos-userdata.sh
@@ -5,7 +5,7 @@ su ubuntu
 cd ~
 
 # install dependencies
-sudo yum install -y make htop openjdk-8-jdk maven
+sudo yum install -y make htop java-1.8.0-openjdk-devel maven
 
 # get the geocity database
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
