2c9d9c371374999a6cb3dd5f46c879f6ed374322
Dylan Mann <mannd@seas.upenn.edu>
2018-04-02 16:40:23 -0400

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-#Make sure that we clone/run as ubuntu
+# Make sure that we clone/run as ubuntu
 su ubuntu
+cd ~
+
+# install dependencies
+sudo apt install make htop openjdk-8-jdk maven
+
+# get the geocity database
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+tar -zxvf GeoLite2-City.tar.gz
+cd `find GeoLite2-City_* | head -1`
+mv *.mmdb ~
 cd ~
 
 # Get latest copy of mldht
