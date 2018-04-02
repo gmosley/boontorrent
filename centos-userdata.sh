7e0f83db29c4bd98154e3752f5ba8c3aacb526bd
Dylan Mann <mannd@seas.upenn.edu>
2018-04-02 17:12:30 -0400

    Create centos-userdata.sh

---
+++ centos-userdata.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Make sure that we clone/run as ubuntu
+su ubuntu
+cd ~
+
+# install dependencies
+sudo yum install -y make htop openjdk-8-jdk maven
+
+# get the geocity database
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+tar -zxvf GeoLite2-City.tar.gz
+cd `find GeoLite2-City_* | head -1`
+mv *.mmdb ~
+cd ~
+
+# Get latest copy of mldht
+git clone https://github.com/dylanmann/mldht.git
+cd mldht
+
+# build code
+mvn package dependency:copy-dependencies appassembler:assemble && mvn antrun:run
+
+# run daemon as root
+cd work
+sleep 10
+(../bin/mldht 0<&- &>> out.log &) & 
