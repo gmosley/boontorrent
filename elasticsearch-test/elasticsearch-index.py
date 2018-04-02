2a50432b458480b26dca345d7b279d5790856308
Graham Mosley <gmosley@seas.upenn.edu>
2018-04-01 21:21:12 -0400

    add number of files and filesize

--- elasticsearch-test/elasticsearch-index.py
+++ elasticsearch-test/elasticsearch-index.py
@@ -7,11 +7,23 @@ data_files = [x for x in data_path.glob('**/*') if x.is_file()]
 
 es = Elasticsearch()
 
+es.indices.delete(index='torrents', ignore=[404])
+
+es.indices.create(
+    index='torrents',
+    body=json.load(open('schema.json'))
+)
+
 for data_file in data_files:
     print(data_file)
     with open(data_file, 'r') as f:
         for line in f:
             r = json.loads(line)
             if r['type'] == 'resolve' and 'name' in r:
-                body = {'name': r['name'], 'infohash': r['infohash']}
+                body = {
+                    'name': r['name'],
+                    'infohash': r['infohash'],
+                    'files': r['files'],
+                    'size': r['size']
+                }
                 es.index(index='torrents', doc_type='_doc', id=r['infohash'], body=body)
